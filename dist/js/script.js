0dd533163781d0d67a72ef1561f840091aeda472
Sebastian <sebzo65@gmail.com>
2021-10-08 21:37:44 +1100

    Managed to fix issue. Now data from API does display. The issue involved using the wrong variable name in the wrong place. API data displays title of book, author name, description and image because I have created elements and text nodes for all of these and appended them to a container. Next task will be to render them within the existing grid I build with HTML so the webpage looks more refined

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,9 +6,8 @@ const GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/v1";
 
 //Async/Await function with fetch that tells JS that the data will be retrieved to be rendered on the webpage,
 //but that there is no guarantee that it will arrive, or set time in which it will arrive
-const getBooks = async (titleBook) => {
-  const response = await fetch(`${GOOGLE_BOOKS_URL}/volumes?q=${titleBook}`);
-  console.log(getBooks);
+const getBooks = async (searchTerm) => {
+  const response = await fetch(`${GOOGLE_BOOKS_URL}/volumes?q=${searchTerm}`);
   const data = await response.json();
   console.log(data);
   return data.items; //.items refers to name of the object holding the data in the API
@@ -17,30 +16,79 @@ const getBooks = async (titleBook) => {
 //Add EventListener for the button which will trigger the API & return search results
 const searchButton = document.querySelector(".searchForm__button");
 searchButton.addEventListener("click", async (e) => {
-  console.log(getBooks("quilts"));
   const searchInput = document.querySelector(".searchForm__input");
-  const titleBook = searchInput.innerHTML; //Here the input value (book title)
+  const searchTerm = searchInput.value; //Here the input value (book title)
   //typed by the user will be appended to API URL in order to find search results
-  if (typeof titleBook === "") {
+  if (searchTerm === "") {
     //If no user input then alert
     alert("Please enter a valid book title");
     return;
   }
 
-  const bookNamesArr = await getBooks(titleBook);
-  console.log(bookNamesArr);
+  const booksArr = await getBooks(searchTerm);
   //Create h2 element for titles of each book and render them in their respective grids
-  const grid_bookTitleItems = bookNamesArr.map((bookName) => {
-    const nameElement = document.createElement("h2");
-    const nameText = `${items.bookName.volumeInfo.title}`;
-    const nameTextNode = document.createTextNode(nameText);
-    console.log(bookName);
-    nameElement.appendChild(nameTextNode);
-    return nameElement;
+  const bookSearchItems = booksArr.map((book) => {
+    //Creating div element that will hold all of the Google Books data user needs
+    const bookContainer = document.createElement("div");
+
+    //Creating heading that will contain title of books
+    const titleElement = document.createElement("h2");
+    const titleText = `${book.volumeInfo.title}`;
+    const titleTextNode = document.createTextNode(titleText);
+
+    //Creating smaller heading that will contain name of author for each book
+    const authorElement = document.createElement("h4");
+    const authorText = `${book.volumeInfo.authors}`;
+    const authorTextNode = document.createTextNode(authorText);
+
+    //Creating img tag that will contain image for each book
+    const imageElement = document.createElement("img");
+    imageElement.src = `${book.volumeInfo.imageLinks.thumbnail}`;
+
+    //Creating paragraph that will contain description for each book
+    const descriptionElement = document.createElement("p");
+    const descriptionText = `${book.volumeInfo.authors}`;
+    const descriptionTextNode = document.createTextNode(descriptionText);
+    console.log(book);
+
+    //Appending text elements to their parent HTML holder
+    titleElement.appendChild(titleTextNode);
+    authorElement.appendChild(authorTextNode);
+    descriptionElement.appendChild(descriptionTextNode);
+
+    //Appending the individual elements(text, author, description, image) to a parent container
+    bookContainer.appendChild(titleElement);
+    bookContainer.appendChild(authorElement);
+    bookContainer.appendChild(descriptionElement);
+    bookContainer.appendChild(imageElement);
+    return bookContainer;
   });
-  console.log(getBooks);
-  const grid_bookTitleList = document.querySelectorAll(".bookNamesArr");
+  const bookGridItems = document.querySelector(".container__bookGrid");
   const append = (parent) => (child) => parent.appendChild(child);
-  grid_bookTitleItems.forEach(append(grid_bookTitleList));
+  bookSearchItems.forEach(append(bookGridItems));
 });
-//Create
+
+// const getBooks = async (searchTerm) => {
+//   const infoArr = info.map((n) => {
+//     const bookObject = {
+//       title:
+//         n.volumeInfo?.title ?? "You sure you got the right title there buddy?",
+//       author:
+//         n.volumeInfo?.author ??
+//         "Either you don't know what you're typing or I ain't a good searchbar. Author unknown",
+//       description:
+//         n.volumeInfo?.description ??
+//         "I literally have no words (and no description) for this book",
+//       img: n.volumeInfo.imageLinks?.thumbnail ?? "../img/default-bookCover.jpg",
+//     };
+//     return bookObject;
+//   });
+//   return infoArr;
+// };
+
+const getTitle = (data) => {
+  const bookTitle = data.items[0].volumeInfo.title;
+  console.log(bookTitle);
+  console.log(data.items.length);
+  return bookTitle;
+};
